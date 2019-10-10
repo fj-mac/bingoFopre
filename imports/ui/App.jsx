3e2bf3dcf85457595bdccf1e4f3109cbead37d16
Francisco Mcallister Ortiz <fj.mac@uniandes.edu.co>
2019-10-09 22:44:48 -0500

    meteor y react funcionando

---
+++ imports/ui/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+const App = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <h1>Bingo Fopre!</h1>
+    </div>
+  );
+};
+
+export default App;
